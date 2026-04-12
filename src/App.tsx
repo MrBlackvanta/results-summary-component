cfbd640864cf355eb184dde9f413fda78b6068ea
Abdelrhman Ahmed <mrblackvanta@gmail.com>
2026-04-12 07:16:45 +0200

    Update main content margin in App component for improved layout consistency

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { ResultSummary } from "components";
 export default function App() {
   return (
     <div className="bg-pale-blue relative grid min-h-dvh w-full grid-cols-1 grid-rows-[1fr_auto] sm:place-content-center">
-      <main className="mx-auto w-full max-w-184 place-self-center">
+      <main className="mx-auto mt-9 w-full max-w-184 place-self-center">
         <ResultSummary />
       </main>
       <footer className="text-dark-gray-blue mt-auto w-full pb-4 text-center text-sm font-light">
